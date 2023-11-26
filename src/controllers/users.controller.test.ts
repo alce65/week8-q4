60ab90e2d4d35fc2a66a8793283f4a3fdcf0ef4e
Serafin Alferez <malaga.carbon3@gmail.com>
2023-11-26 19:46:55 +0100

    Add controller test

--- src/controllers/users.controller.test.ts
+++ src/controllers/users.controller.test.ts
@@ -90,11 +90,6 @@ describe('Given UsersController class', () => {
       expect(mockNext).toHaveBeenLastCalledWith(mockError);
     });
 
-    // Ttest('Then search should ...', async () => {
-    //   await controller.search(mockRequest, mockResponse, mockNext);
-    //   expect(mockNext).toHaveBeenLastCalledWith(mockError);
-    // });
-
     test('Then create should ...', async () => {
       await controller.create(mockRequest, mockResponse, mockNext);
       expect(mockNext).toHaveBeenLastCalledWith(mockError);
@@ -109,5 +104,35 @@ describe('Given UsersController class', () => {
       await controller.delete(mockRequest, mockResponse, mockNext);
       expect(mockNext).toHaveBeenLastCalledWith(mockError);
     });
+
+      test('Then login should...', async () => {
+    const mockUserId = 'mockUserId';
+    const mockLoginResult = { id: 'mockUserId', email: 'mock@example.com' };
+
+    // Mocking the request with a userId
+    const mockRequest = {
+      body: { userId: mockUserId },
+    } as unknown as Request;
+
+    // Mocking the repo methods for both cases
+    const mockRepo = {
+      getById: jest.fn().mockResolvedValue(mockLoginResult),
+      login: jest.fn().mockResolvedValue(mockLoginResult),
+    } as unknown as UsersMongoRepo;
+
+    // Creating two instances of the controller
+    const controller = new UsersController(mockRepo);
+
+    // Testing with userId
+    await controller.login(mockRequest, mockResponse, mockNext);
+    expect(mockRepo.getById).toHaveBeenCalledWith(mockUserId);
+    expect(mockResponse.status).toHaveBeenCalledWith(202);
+    expect(mockResponse.statusMessage).toBe('Accepted');
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      user: mockLoginResult,
+      token: expect.any(String),
+    });
+  });
+
   });
 });
